ef189097701f06f3c4a72f3e201d2bdf244c7102
Oleg Obleukhov <leoleovich@fb.com>
2019-03-28 18:20:28 +0000

    connecting instead of scanning

--- script.sh
+++ script.sh
@@ -1,20 +1,23 @@
 device_mac="12-34-56-78-90-10"
 logfile="/tmp/watchlock"
+check_interval=10
 
 # Be sure we don't run second instance
-file_recently_touched=$(find /tmp/watchlock -mmin -1 -print)
+sleep $(echo $RANDOM%10 | bc)
+file_recently_touched=$(find $logfile -mmin -1 -print)
 if [ ! -z "$file_recently_touched" ]
 then
 	echo "Seems like there is another instance running. Exiting"
-	exit
+	exit 1
 fi
+echo "First run" > $logfile
 
 {
 locked="NO"
 while true
 do
 	date
-	visible=$(/usr/local/bin/blueutil --inquiry 10 | grep $device_mac  || echo NO)
+	visible=$(/usr/local/bin/blueutil --connect $device_mac || echo NO)
 	if [ "$visible" == "NO" ]
 	then
 		echo "BT device disappeared"
@@ -25,11 +28,11 @@ do
 			/System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine
 		fi
 	else
-		echo $visible
 		echo "BT device is visible"
 		locked="NO"
+		sleep $check_interval
 	fi
 	# Avoid CPU crashing loop
-	sleep 1
+	sleep 0.1
 done
-} >> /tmp/watchlock
+} >> $logfile
